e3533cdfaf75581bd3f6088f8ef4fdf11720b90a
akshaykn <akshaykrishna633@gmail.com>
2022-03-25 00:05:53 +0530

    fifth commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs');
 var fs = require('fs');
-const port = 8000;   
+const port = process.env.PORT || 80;   
 app.use(express.static(__dirname + '/assets'));
 
 // Render index.ejs file
